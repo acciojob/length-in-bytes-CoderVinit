8e139f255f3e9b1921bbbe3d308ddbf40850faed
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-01 16:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	const encoder = new TextEncoder();
+	const byteArray = encoder.encode(str);
+  return byteArray.length;
+	
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
